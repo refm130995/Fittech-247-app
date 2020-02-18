42caad9599fcdc7af06061329db3606979f68d50
brayan arias <bryanjose84@hotmail.com>
2020-02-17 21:53:47 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -9,11 +15,30 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+var firebaseConfig = {
+  apiKey: "AIzaSyCJ6x0dEis-hQbWGO0yDOd-yjW2WLBR5x8",
+  authDomain: "login-32a05.firebaseapp.com",
+  databaseURL: "https://login-32a05.firebaseio.com",
+  projectId: "login-32a05",
+  storageBucket: "login-32a05.appspot.com",
+  messagingSenderId: "852806702714",
+  appId: "1:852806702714:web:34bba892e3d1693023cdc3",
+  measurementId: "G-WK88CRWK4F"
+}
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+  ],
   providers: [
+    GooglePlus,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase/app'
+import { AngularFireAuth } from '@angular/fire/auth';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { Platform } from '@ionic/angular';
+import { Observable } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +13,54 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    public logeado:Observable<firebase.User> ;
 
-  constructor() {}
+  constructor(public afAuth:AngularFireAuth,
+              private gplus:GooglePlus,
+              private platform:Platform) {
+                this.logeado = this.afAuth.authState
+              }
+
+
+  googleLogin(){
+    if(this.platform.is('cordova')){
+      this.nativegooglelogin()
+    }else{
+      this.webgooglelogin()
+    }
+  }
+
+  async nativegooglelogin(){
+    try{
+      const gplusUser = await this.gplus.login({
+        'scopes': '', 
+        'webClientId':'852806702714-hha82fck69qmatnl5ufm806hknfo7utf.apps.googleusercontent.com',
+        'offline': true
+      })
+
+      return await this.afAuth.auth.signInWithCredential(
+        firebase.auth.GoogleAuthProvider.credential(gplusUser.idToken)
+      )
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  async webgooglelogin(){
+    try{
+      const provider = new firebase.auth.GoogleAuthProvider()
+      const credential = await this.afAuth.auth.signInWithPopup(provider)
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+
+  signOut(){
+    this.afAuth.auth.signOut()
+    if(this.platform.is('cordova')){
+      this.gplus.logout()
+    }
+  }
 
 }
