296e8d09e98e0b3120a7b5ba3e3ce4e806ae7c7e
bryan <bryanjose84@hotmail.com>
2020-05-20 18:07:59 -0400

    cambios finales 20/05/2020

--- src/app/services/nutricion.service.ts
+++ src/app/services/nutricion.service.ts
@@ -76,14 +76,14 @@ export class NutricionService {
   }
 
   getFoods(){
-    return new Promise( (resolve, reject)  => {
+    return new Promise( async (resolve, reject)  => {
       const headers = new HttpHeaders({
-        'Authorization': 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTY3MDZkZjY3NGNiODdkMjkzNDU4ZjVmZWU5N2YzYTg0ZTVmYWVhNmZiYmI5YjQzMTVmOTQyYTc4NGQ3YWM2ZGY1MTRiMThlYTE1M2QxNzYiLCJpYXQiOjE1ODk5OTQxNzAsIm5iZiI6MTU4OTk5NDE3MCwiZXhwIjoxNjIxNTMwMTcwLCJzdWIiOiIyNCIsInNjb3BlcyI6W119.trbx79f5TsF4uSY3O78wTN_LCyk93EV2g9NyGyATZ41VDX6Pv38LI5p2nKaTGIRjmQ2-ODV_lh74XFdR1aznv5qvLDkmTVTRcvNwoN8UPi7QM5Zjf_23xfrjJnnxxNCvSRLwjHi0f8fm1yZ_NZUPnaExKwsyNoJpzvoQGpaPtsPLev9JyKUBiFogB3--sKxcWTNKo9RXrrmZyqvDthSnu3KslwEDFB6vAljPXUdSyQhU5xlKPBNGrQzSaXQgGwR5oLBAnTndUEs0O0Yf31ACTtt3LCzeAi7rxmlDEKqr-hDgln8-r3ks99g-g_7pLrW0e_-nljGPpuwMUanRPLCUqaMuUHqPw6D9urfqhJd0cDMYoLPol8YvrkIVADzYUo4p7KXIA7Kg0GASOfRfaYeQL00GVsbKrQuCW682MQRtL4tjG0CP56INMr8rinDZLFdstMGWAaSKum6sdw45zH3LaqGSB3LuroF1ChbWINV9wQ9tCBaCoWQMiSD1EkZIinmhMQk7QExVid6eEX80U-KAAvqOxnVoERZ-FWl--WOgWpDpFAbwmLQUw7VAWgbDX4k7QygpOjbOZxs8ggKhBKcYr0F8dOHcaBfUt7OvSZeSMKiDwb0kZQ_r78dEhNjdnDozAhs4ihzexBB6vs7dglNw06ghvfRIwlGQwkSQzo-OUqs',
+        'Authorization': 'Bearer ' + await this.service.cargarToken(),
         'Content-Type':'application/json',
-      })
-      this.http.post('http://fittech247.com/fittech/api/auth/foods',{headers})
+      })  
+      this.http.post(`${URL}/auth/foods`,{headers})
           .subscribe(resp=>{
-            console.log(resp)
+          
             resolve(true)
           },err=>{
             reject(false)
